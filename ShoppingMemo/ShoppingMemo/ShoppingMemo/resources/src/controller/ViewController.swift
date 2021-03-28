d7dedb225a4247eca0da05e40ccf996f41dd5993
okateru0217 <okateru.0217@gmail.com>
2021-03-28 20:58:13 +0900

    Add:tableView
    tableViewを追加。
    cellをタップした時の処理に手間取り中。

--- ShoppingMemo/ShoppingMemo/ShoppingMemo/resources/src/controller/ViewController.swift
+++ ShoppingMemo/ShoppingMemo/ShoppingMemo/resources/src/controller/ViewController.swift
@@ -2,25 +2,26 @@
 //  ViewController.swift
 //  ShoppingMemo
 //
-//  Created by 岡澤輝明 on 2021/03/28.
+//  Created by on 2021/03/28.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var shoppingMemoTableView: UITableView!
     @IBOutlet weak var addMemoTextField: UITextField!
-
+    
+    var shoppingMemo = ["cell"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // ナビゲーションバーの背景色を変更する
         changeNavigationBarLook()
-        // キーボードを開いた時、テキストフィールドを一緒に上へ上げる処理
-        keyboardWillChangeFrameNotification()
-        // キーボードを閉じた時、テキストフィールドを一緒に下へ下げる処理
-        keyboardWillHideNotification()
+        textFieldDelegateSelf()
         // タップでキーボードを閉じる処理
         closeInTap()
+        shoppingMemoTableView.tableFooterView = UIView(frame: .zero)
         // Do any additional setup after loading the view.
     }
     
@@ -28,6 +29,6 @@ class ViewController: UIViewController {
     func changeNavigationBarLook() {
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.28, green: 0.53, blue: 0.75, alpha: 1.0)
     }
-
+    
 }
 
